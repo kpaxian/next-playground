010fd29d32d7165088831957e4e7422f35e52c77
George Serban <george.serban26@gmail.com>
2024-06-02 18:28:12 +0300

    metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,15 @@
+import { Metadata } from 'next';
 interface ProductDetailsProps {
     params: {
         productId: string
     }
-} 
+}
+
+ export const generateMetadata = ({ params }: ProductDetailsProps): Metadata => {
+    return {
+        title: `Product ${params.productId}`
+    }
+ } 
 
 function ProductDetails ({ params }: ProductDetailsProps) {
     return <h2>Details about the product {params.productId}</h2>
