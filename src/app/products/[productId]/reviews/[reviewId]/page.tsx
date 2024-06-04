bcc4ff6eaafdd577d23373e5b46166055483d45e
George Serban <george.serban26@gmail.com>
2024-06-04 13:59:06 +0300

    some error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { notFound } from "next/navigation";
 interface ReviewDetailProps {
     params: {
@@ -6,7 +8,15 @@ interface ReviewDetailProps {
     }
 }
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail({params}: ReviewDetailProps) {
+    const random = getRandomInt(2)
+    if (random === 1) {
+        throw new Error('Error loading review');
+    }
     if (parseInt(params.reviewId) > 1000) {
         notFound()
     }
