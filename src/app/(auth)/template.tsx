30dd252cd955044b3f52d997ead08712033cab15
George Serban <george.serban26@gmail.com>
2024-06-04 11:51:28 +0300

    adding template an loading

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from 'react';
 
 const navLinks = [
     {name: 'Register', href: '/register'},
@@ -14,8 +15,12 @@ export default function AuthLayout({
     children: React.ReactNode
 }) {
     const pathname = usePathname();
+    const [input, setInput] = useState('');
     return (
         <div>
+            <div style={{border: '1px solid grey'}}>
+                <input style={{color: 'black'}} value={input} onChange={e => setInput(e.target.value)}/>
+            </div>
             {navLinks.map((link) => {
                 const isActive = pathname.startsWith(link.href);
                 return (
