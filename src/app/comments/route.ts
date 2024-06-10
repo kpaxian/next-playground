d5f578d69aa5c3fde5427acad1f22f6c2b2794c3
George Serban <george.serban26@gmail.com>
2024-06-10 17:16:24 +0300

    query parametrs

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,7 +1,15 @@
 import { comments } from "./data";
+import { type NextRequest } from "next/server";
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('query');
+
+    const filteredComments = query
+        ? comments.filter(item => item.text.includes(query))
+        : comments
+
+    return Response.json(filteredComments);
 }
 export async function POST(req: Request) {
     const comment = await req.json();
