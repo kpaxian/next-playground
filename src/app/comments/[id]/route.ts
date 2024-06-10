6ad408edf9f14199758a5ce8f92f2f66cfd47251
George Serban <george.serban26@gmail.com>
2024-06-10 17:20:59 +0300

    apply redirection

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,9 +1,14 @@
+import { redirect } from "next/navigation";
 import { comments } from "../data"
 
 export async function GET(
     _request: Request,
     { params }: { params: { id: string } }
 ) {
+
+    if (parseInt(params.id) > comments.length) {
+        redirect('/comments')
+    }
     const comment = comments.find(
         item => item.id === parseInt(params.id)   
     );
